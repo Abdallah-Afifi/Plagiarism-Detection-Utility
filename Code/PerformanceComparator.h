2735b3cfc7f0388fc51fb902bc5b8fd0ab89351a
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-05-13 21:36:12 +0300

    Delete Code directory

--- Code/PerformanceComparator.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PERFORMANCECOMPARATOR_H
-#define _PERFORMANCECOMPARATOR_H
-
-class PerformanceComparator {
-public: 
-void compare(string algorithm1, string algorithm2, Document testDoc, Corpus corpus);
-};
-
-#endif //_PERFORMANCECOMPARATOR_H
--- Code/RabinKarpDetector.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RABINKARPDETECTOR_H
-#define _RABINKARPDETECTOR_H
-
-class RabinKarpDetector {
-public:    
-void detectPlagiarism(Document testDoc, Corpus corpus);
-};
-
-#endif //_RABINKARPDETECTOR_H
